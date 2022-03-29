e187c5c5f03068f1bb39a805c8e3db90d0f2e565
TahirMansuri <tprocks786@gmail.com>
2022-03-29 21:59:37 +0530

    MODIFIED: Adding Logger Message usign SLF4J

--- src/main/java/com/demo/springconcepts/SpringConceptsApplication.java
+++ src/main/java/com/demo/springconcepts/SpringConceptsApplication.java
@@ -2,6 +2,8 @@ package com.demo.springconcepts;
 
 import com.demo.springconcepts.component.DemoBean;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -9,12 +11,16 @@ import org.springframework.context.ApplicationContext;
 @SpringBootApplication
 public class SpringConceptsApplication {
 
+	public static final Logger logger = LoggerFactory.getLogger(SpringConceptsApplication.class);
+
 	public static void main(String[] args) {
-		System.out.println("Welcome to Sprint Boot Concept Demo");
+		//System.out.println("Welcome to Sprint Boot Concept Demo");
+		logger.debug("Welcome to Sprint Boot Concept Demo");
 		//SpringApplication.run(SpringConceptsApplication.class, args);
 		ApplicationContext context = SpringApplication.run(SpringConceptsApplication.class, args);
 		DemoBean demoBean = context.getBean(DemoBean.class);
-		System.out.println("DemoBean = "+demoBean);
+		//System.out.println("DemoBean = "+demoBean);
+		logger.debug("DemoBean = "+demoBean);
 	}
 
 }
